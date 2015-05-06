4c7569e356db238b0fad69c951f87c6a47729e4e
lin-li <linli.cs@gmail.com>
2015-05-06 16:49:43 -0700

    Launch DetailActivity

--- app/src/main/java/com/example/linli/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/linli/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.linli.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -91,7 +93,13 @@ public class ForecastFragment extends Fragment {
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
 
-        // new FetchWeatherTask().execute("90007");
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent detailIntent = new Intent(getActivity(), DetailActivity.class);
+                startActivity(detailIntent);
+            }
+        });
 
         return rootView;
     }
